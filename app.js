1fecc8d238a645a77446850924eb2cfe91242d2f
Muhammadfayyozbek Ahmadjonov <usmonovbahriddin108@gmail.com>
2024-05-12 15:46:50 +0500

    complete

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const showBtnEl = document.getElementById("showBtn")
-const modalEL = document.getElementById("modal")
-const closeBtnEl = document.getElementById("closeBtn")
-const overlayEl = document.getElementById("overlay")
+const showBtn = document.getElementById("showBtn")
+const modal = document.getElementById("modal")
+const closeBtn = document.getElementById("closeBtn")
+const overlay = document.getElementById("overlay")
 const addHidden = () => {
-    modalEL.classList.add("hidden")
-    overlayEl.classList.add("hidden")
+    modal.classList.add("hidden")
+    overlay.classList.add("hidden")
 }
 const removeHidden = () => {
-    modalEL.classList.remove("hidden")
-    overlayEl.classList.remove("hidden")    
+    modal.classList.remove("hidden")
+    overlay.classList.remove("hidden")
 }
-showBtnEl.addEventListener("click",removeHidden())
-closeBtnEl.addEventListener("click", addHidden())
-overlayEl.addEventListener("click", addHidden())
+showBtn.addEventListener("click",removeHidden)
+closeBtn.addEventListener("click", addHidden)
+overlay.addEventListener("click", addHidden)
 document.addEventListener('keydown',(e)=>{
     if(e.key == "Escape"){
         addHidden()
